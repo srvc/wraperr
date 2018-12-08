c2d104819aa71736974cc68c1c9d62e450d3d8ed
izumin5210 <m@izum.in>
2018-12-08 20:02:35 +0900

    Refactor

--- analyzer.go
+++ analyzer.go
@@ -1,12 +1,8 @@
 package wraperr
 
 import (
-	"bufio"
 	"go/ast"
 	"go/token"
-	"go/types"
-	"os"
-	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -57,177 +53,22 @@ func init() {
 }
 
 func runAnalyze(pass *analysis.Pass) (interface{}, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	r := newFileReader(pass.Fset)
 
-	isWrapped := func(call *ast.CallExpr) bool {
-		switch fexpr := call.Fun.(type) {
-		case *ast.SelectorExpr:
-			if f, ok := pass.TypesInfo.ObjectOf(fexpr.Sel).(*types.Func); ok {
-				chunks := []string{}
-				for _, chunk := range strings.Split(f.FullName(), "/") {
-					if chunk == "vendor" {
-						chunks = []string{}
-					} else {
-						chunks = append(chunks, chunk)
-					}
-				}
-				_, ok = wrapperFuncSet[strings.Join(chunks, "/")]
-				return ok
-			}
-		}
-		return false
+	reportFunc := func(assignedAt, returnedAt token.Pos) {
+		occPos := pass.Fset.Position(assignedAt)
+		line := sprintInlineCode(r.GetLine(assignedAt))
+		pass.Reportf(returnedAt, "the error is assigned on L%d: %s", occPos.Line, line)
 	}
 
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 	nodeFilter := []ast.Node{
 		(*ast.FuncDecl)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		f := n.(*ast.FuncDecl)
-		if f.Type == nil || f.Type.Results == nil {
-			return
-		}
-
-		fields := f.Type.Results.List
-		errInReturn := make([]bool, 0, 2*len(fields))
-		errNames := make(map[string]struct{}, 2*len(fields))
-		errIdents := map[string]*errIdent{}
-		var ok bool
-		for _, f := range fields {
-			isErr := isErrorType(pass.TypesInfo.TypeOf(f.Type))
-			ok = ok || isErr
-			if len(f.Names) == 0 {
-				errInReturn = append(errInReturn, isErr)
-			}
-			for _, n := range f.Names {
-				errInReturn = append(errInReturn, isErr)
-				errNames[n.Name] = struct{}{}
-			}
-		}
-		if !ok {
-			return
-		}
-
-		lines := make(map[string][]string)
-
-		// ref: https://github.com/kisielk/errcheck/blob/1787c4bee836470bf45018cfbc783650db3c6501/internal/errcheck/errcheck.go#L488-L498
-		getLine := func(tp token.Pos) string {
-			pos := pass.Fset.Position(tp)
-			foundLines, ok := lines[pos.Filename]
-
-			if !ok {
-				f, err := os.Open(pos.Filename)
-				if err == nil {
-					sc := bufio.NewScanner(f)
-					for sc.Scan() {
-						foundLines = append(foundLines, sc.Text())
-					}
-					lines[pos.Filename] = foundLines
-					f.Close()
-				}
-			}
-
-			line := "??"
-			if pos.Line-1 < len(foundLines) {
-				line = strings.TrimSpace(foundLines[pos.Line-1])
-			}
-
-			return line
-		}
-
-		recordUnwrappedError := func(occurredAt, returnedAt token.Pos) {
-			occPos := pass.Fset.Position(occurredAt)
-			pass.Reportf(returnedAt, "the error is assigned on L%d: %s", occPos.Line, sprintInlineCode(getLine(occurredAt)))
-		}
-
-		ast.Inspect(f, func(n ast.Node) bool {
-			switch stmt := n.(type) {
-			case *ast.AssignStmt:
-				var errIds []*ast.Ident
-				for _, expr := range stmt.Lhs {
-					if !isErrorType(pass.TypesInfo.TypeOf(expr)) {
-						continue
-					}
-					if id, ok := expr.(*ast.Ident); ok {
-						errIds = append(errIds, id)
-					}
-				}
-				if len(errIds) > 0 {
-					// Detect wrapped error assignment
-					var wrapped bool
-					for _, expr := range stmt.Rhs {
-						if cexpr, ok := expr.(*ast.CallExpr); ok {
-							if isWrapped(cexpr) {
-								wrapped = true
-							}
-						}
-					}
-					for _, id := range errIds {
-						errIdents[id.Name] = &errIdent{Ident: id, wrapped: wrapped}
-					}
-				}
-			case *ast.ReturnStmt:
-				switch len(stmt.Results) {
-				case 0:
-					// Named return values
-					for n := range errNames {
-						if errIdent, ok := errIdents[n]; ok && !errIdent.wrapped {
-							recordUnwrappedError(errIdent.Pos(), stmt.Return)
-						}
-					}
-				case len(errInReturn):
-					// Simple return
-					for i, expr := range stmt.Results {
-						if !errInReturn[i] {
-							continue
-						}
-						switch expr := expr.(type) {
-						case *ast.Ident:
-							if errIdent, ok := errIdents[expr.Name]; ok && !errIdent.wrapped {
-								recordUnwrappedError(errIdent.Pos(), expr.NamePos)
-							}
-						case *ast.CallExpr:
-							if !isWrapped(expr) {
-								recordUnwrappedError(expr.Pos(), expr.Lparen)
-							}
-						default:
-							// TODO: should report unexpected exper
-						}
-					}
-				case 1:
-					// Return another function directly
-					switch expr := stmt.Results[0].(type) {
-					case *ast.CallExpr:
-						if !isWrapped(expr) {
-							recordUnwrappedError(expr.Pos(), expr.Pos())
-						}
-					default:
-						// TODO: should report unexpected exper
-					}
-				default:
-					// TODO: should report unexpected exper
-				}
-			}
-			return true
-		})
+		NewChecker(pass.Fset, pass.TypesInfo, n.(*ast.FuncDecl)).Check(reportFunc)
 	})
 
 	return nil, nil
-}
-
-func sprintInlineCode(s string) string {
-	cc := 1
-	c := cc
-	for _, r := range s {
-		if r == '`' {
-			cc++
-			if cc > c {
-				c = cc
-			}
-		} else {
-			cc = 1
-		}
-	}
-	q := strings.Repeat("`", c)
-	return q + s + q
 }
---
+++ checker.go
@@ -0,0 +1,166 @@
+package wraperr
+
+import (
+	"go/ast"
+	"go/token"
+	"go/types"
+	"strings"
+)
+
+type Checker interface {
+	Check(ReportFunc)
+}
+
+type ReportFunc func(assignedAt, returnedAt token.Pos)
+
+func NewChecker(
+	fset *token.FileSet,
+	info *types.Info,
+	f *ast.FuncDecl,
+) Checker {
+	return &checkerImpl{
+		fset: fset,
+		info: info,
+		f:    f,
+	}
+}
+
+type checkerImpl struct {
+	fset *token.FileSet
+	info *types.Info
+	f    *ast.FuncDecl
+
+	errInReturn []bool
+	errNames    map[string]struct{}
+	errIdents   map[string]*errIdent
+}
+
+func (c *checkerImpl) Check(report ReportFunc) {
+	if !c.init() {
+		return
+	}
+
+	ast.Inspect(c.f, func(n ast.Node) bool {
+		switch stmt := n.(type) {
+		case *ast.AssignStmt:
+			c.checkAssignment(stmt)
+		case *ast.ReturnStmt:
+			c.checkReturn(stmt, report)
+		}
+		return true
+	})
+}
+
+func (c *checkerImpl) init() (ok bool) {
+	if c.f.Type == nil || c.f.Type.Results == nil {
+		return
+	}
+
+	fields := c.f.Type.Results.List
+
+	c.errInReturn = make([]bool, 0, 2*len(fields))
+	c.errNames = make(map[string]struct{}, 2*len(fields))
+	c.errIdents = map[string]*errIdent{}
+
+	for _, f := range fields {
+		isErr := isErrorType(c.info.TypeOf(f.Type))
+		ok = ok || isErr
+		if len(f.Names) == 0 {
+			c.errInReturn = append(c.errInReturn, isErr)
+		}
+		for _, n := range f.Names {
+			c.errInReturn = append(c.errInReturn, isErr)
+			c.errNames[n.Name] = struct{}{}
+		}
+	}
+
+	return
+}
+
+func (c *checkerImpl) checkAssignment(stmt *ast.AssignStmt) {
+	var errIds []*ast.Ident
+	for _, expr := range stmt.Lhs {
+		if !isErrorType(c.info.TypeOf(expr)) {
+			continue
+		}
+		if id, ok := expr.(*ast.Ident); ok {
+			errIds = append(errIds, id)
+		}
+	}
+	if len(errIds) > 0 {
+		// Detect wrapped error assignment
+		var wrapped bool
+		for _, expr := range stmt.Rhs {
+			if cexpr, ok := expr.(*ast.CallExpr); ok {
+				if c.isWrapped(cexpr) {
+					wrapped = true
+				}
+			}
+		}
+		for _, id := range errIds {
+			c.errIdents[id.Name] = &errIdent{Ident: id, wrapped: wrapped}
+		}
+	}
+}
+
+func (c *checkerImpl) checkReturn(stmt *ast.ReturnStmt, report ReportFunc) {
+	switch len(stmt.Results) {
+	case 0:
+		// Named return values
+		for n := range c.errNames {
+			if errIdent, ok := c.errIdents[n]; ok && !errIdent.wrapped {
+				report(errIdent.Pos(), stmt.Return)
+			}
+		}
+	case len(c.errInReturn):
+		// Simple return
+		for i, expr := range stmt.Results {
+			if !c.errInReturn[i] {
+				continue
+			}
+			switch expr := expr.(type) {
+			case *ast.Ident:
+				if errIdent, ok := c.errIdents[expr.Name]; ok && !errIdent.wrapped {
+					report(errIdent.Pos(), expr.NamePos)
+				}
+			case *ast.CallExpr:
+				if !c.isWrapped(expr) {
+					report(expr.Pos(), expr.Lparen)
+				}
+			default:
+				// TODO: should report unexpected exper
+			}
+		}
+	case 1:
+		// Return another function directly
+		switch expr := stmt.Results[0].(type) {
+		case *ast.CallExpr:
+			if !c.isWrapped(expr) {
+				report(expr.Pos(), expr.Pos())
+			}
+		default:
+			// TODO: should report unexpected exper
+		}
+	default:
+		// TODO: should report unexpected exper
+	}
+}
+
+func (c *checkerImpl) isWrapped(call *ast.CallExpr) bool {
+	switch fexpr := call.Fun.(type) {
+	case *ast.SelectorExpr:
+		if f, ok := c.info.ObjectOf(fexpr.Sel).(*types.Func); ok {
+			chunks := []string{}
+			for _, chunk := range strings.Split(f.FullName(), "/") {
+				if chunk == "vendor" {
+					chunks = []string{}
+				} else {
+					chunks = append(chunks, chunk)
+				}
+			}
+			_, ok = wrapperFuncSet[strings.Join(chunks, "/")]
+			return ok
+		}
+	}
+	return false
+}
